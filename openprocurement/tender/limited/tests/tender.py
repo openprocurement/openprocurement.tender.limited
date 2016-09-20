6cf461da28d581cbc707ccee04a96a925c535f55
ivan1233211 <ivanrouman@gmail.com>
2016-09-20 18:29:16 +0300

    Check if contract is without awards

--- openprocurement/tender/limited/tests/tender.py
+++ openprocurement/tender/limited/tests/tender.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 from copy import deepcopy
+from uuid import uuid4
 
 from openprocurement.api import ROUTE_PREFIX
 from openprocurement.api.models import get_now
@@ -254,6 +255,23 @@ class TenderResourceTest(BaseTenderWebTest):
         self.assertEqual(response.status, '200 OK')
         self.assertEqual(len(response.json['data']), 4)
 
+    def test_tender_award_create(self):
+        data = test_tender_data.copy()
+        award_id = uuid4().hex
+        data['awards'] = [{'suppliers': [test_organization],
+                           'subcontractingDetails': 'Details',
+                           'status': 'pending',
+                           'qualified': True,
+                           'id': award_id}
+                           ]
+        
+        data['contracts'] = [{'title': 'contract title', 'description': 'contract description', 'awardID' : award_id}]
+        response = self.app.post_json('/tenders', {'data': data})
+        tender_id = response.json['data']['id']
+        response = self.app.get('/tenders/{}'.format(tender_id))
+        self.assertEqual(response.status, '200 OK') 
+        self.assertFalse('contracts' in response.json['data'] and not 'awards' in response.json['data']) 
+        
     def test_listing_changes(self):
         response = self.app.get('/tenders?feed=changes')
         self.assertEqual(response.status, '200 OK')
